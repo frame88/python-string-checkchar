5d29f6eaf1ef31962ec5bbbc3b1df3ddde44db93
frame88 <fleuzzi97@gmail.com>
2022-02-28 19:42:07 +0100

    cambio messaggio come richiesto nella traccia

--- 2.2.py
+++ 2.2.py
@@ -3,6 +3,6 @@ word_1 = input('inserisci una stringa: ')
 print('controllo se l\'ultima e la prima lettera sono ugali \n')
 
 if word_1[0] == word_1[-1]:
-    print('Il primo e l\'ultimo carattere sono uguali!!!')
+    print('caratteri iniziale e finale uguali')
 else:
-    print('primo e ultimo carattere non coincidono')
+    print('caratteri iniziale e finale diversi')
