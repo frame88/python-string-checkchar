4c1ec3f939ca290163460e4bafc1f41585f9efc5
frame88 <fleuzzi97@gmail.com>
2022-02-28 19:40:14 +0100

    add conditional if

--- 2.2.py
+++ 2.2.py
@@ -1,10 +1,8 @@
-'''
-Scrivere un programma che prende in ingresso una stringa s non vuota e verifica se il primo e l’ultimo
-carattere sono uguali. In caso positivo stampa “caratteri iniziale e finale uguali” altrimenti stampa “caratteri
-iniziale e finale diversi”. Ad esempio, se s =“ambasciata” il programma deve stampare “caratteri iniziale e
-finale uguali”.
-'''
-
 word_1 = input('inserisci una stringa: ')
 
-print('controllo se l\ultima e la prima lettera sono ugali)
+print('controllo se l\'ultima e la prima lettera sono ugali \n')
+
+if word_1[0] == word_1[-1]:
+    print('Il primo e l\'ultimo carattere sono uguali!!!')
+else:
+    print('primo e ultimo carattere non coincidono')
