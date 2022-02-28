ef460c26e3567bc029c6b4c7f67d7f296e709b79
frame88 <fleuzzi97@gmail.com>
2022-02-28 19:34:25 +0100

    input string

---
+++ 2.2.py
@@ -0,0 +1,10 @@
+'''
+Scrivere un programma che prende in ingresso una stringa s non vuota e verifica se il primo e l’ultimo
+carattere sono uguali. In caso positivo stampa “caratteri iniziale e finale uguali” altrimenti stampa “caratteri
+iniziale e finale diversi”. Ad esempio, se s =“ambasciata” il programma deve stampare “caratteri iniziale e
+finale uguali”.
+'''
+
+word_1 = input('inserisci una stringa: ')
+
+print('controllo se l\ultima e la prima lettera sono ugali)
